cc40d1194a24d866080130e5bfc6301d2b107c78
ncb6206 <ncb6206@gmail.com>
2023-11-05 23:39:40 +0900

    Init : 프로젝트 초기설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-import type { Metadata } from "next";
-import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import React from 'react';
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
 
-const font = Noto_Sans_KR({ subsets: ["latin"] });
+const font = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "밥정너",
-  description: "밥은 정해져있고 너만 답하면 돼",
+  title: '밥정너',
+  description: '밥은 정해져있고 너만 답하면 돼',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={font.className}>{children}</body>
