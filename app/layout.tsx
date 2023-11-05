975deac308ffeab8fadf4e709d4975edc643df2c
ncb6206 <ncb6206@gmail.com>
2023-11-06 03:27:55 +0900

    Feat : 전체적인 레이아웃, 초기화면 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
+
 import './globals.css';
+import Toast from '@/app/components/Toast';
 
 const font = Noto_Sans_KR({ subsets: ['latin'] });
 
@@ -17,7 +19,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <div
+          id="container"
+          className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] overflow-y-hidden bg-[#282828]"
+        >
+          {children}
+          <Toast />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>밥정너?</div>;
+import NaverLoginButton from '@/app/components/NaverLoginButton';
+
+export default function Page() {
+  return (
+    <main className="flex h-full flex-col text-white">
+      <div className="mx-auto mt-4">밥정너</div>
+      <div className="flex-1"></div>
+      <div className="p-2 ">
+        <NaverLoginButton />
+      </div>
+    </main>
+  );
 }
